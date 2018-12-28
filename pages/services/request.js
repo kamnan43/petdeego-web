756c23794bbacc31083ef19f97a001cabf0b40b5
bmqdev <bmqdev@gmail.com>
2018-12-28 21:37:45 +0700

    update style

--- pages/services/request.js
+++ pages/services/request.js
@@ -5,7 +5,7 @@ import dynamic from 'next/dynamic';
 import swal from 'sweetalert2';
 import DefaultLayout from '../../components/layout/DefaultLayout';
 import Header from 'components/form/Header';
-// import { isolateGlobalState } from 'mobx/lib/internal';
+import { datetime } from '../../src/utils/datetime';
 
 const GMapPicker = dynamic(import('../../components/mappicker/GMapPicker'), {
   ssr: false
@@ -117,6 +117,7 @@ class Request extends Component {
 
   render() {
     const service = this.props.service.toJS().data;
+    const date = datetime.moment(service.date).format('YYYY-MM-DD')
     return (
       <Fragment>
         <NextHead>
@@ -130,7 +131,7 @@ class Request extends Component {
               <div className="col-sm-6">
                 <div className="form-check">
                   <input className="form-check-input" type="checkbox" id="dog" onClick={this.changePetType.bind(this, 'dog')} value={service.type_dog} />
-                  <label className="form-check-label" htmlFor="dog">
+                  <label className="form-check-label p-l-5" htmlFor="dog">
                     สุนัข
                   </label>
                 </div>
@@ -138,96 +139,96 @@ class Request extends Component {
               <div className="col-sm-6">
                 <div className="form-check">
                   <input className="form-check-input" type="checkbox" id="cat" onClick={this.changePetType.bind(this, 'cat')} value={service.type_cat} />
-                  <label className="form-check-label" htmlFor="cat">
+                  <label className="form-check-label p-l-5" htmlFor="cat">
                     แมว
                   </label>
                 </div>
               </div>
             </div>
 
-              <div className="form-group col-sm-12">
-                <label>จำนวนสัตว์เลี้ยง</label>
-                <input type="number" className="form-control" id="qty" placeholder="" onChange={e => {
-                  this.setVal('qty', +e.target.value)
+            <div className="form-group col-sm-12">
+              <label>จำนวนสัตว์เลี้ยง</label>
+              <input type="number" className="form-control" id="qty" placeholder="" onChange={e => {
+                this.setVal('qty', +e.target.value)
+              }} />
+            </div>
+
+            <div className="form-group col-sm-12">
+              <label>ขนาดสัตว์เลี้ยง</label>
+              <input type="text" className="form-control" id="exampleInputPassword1" placeholder="S, M, L, XL"
+                onChange={e => {
+                  this.setVal('sizes', e.target.value)
                 }} />
-              </div>
+            </div>
 
-              <div className="form-group col-sm-12">
-                <label>ขนาดสัตว์เลี้ยง</label>
-                <input type="text" className="form-control" id="exampleInputPassword1" placeholder="S, M, L, XL"
-                  onChange={e => {
-                    this.setVal('sizes', e.target.value)
-                  }} />
-              </div>
-
-              <div className="form-group col-sm-12">
-                <div className="col-sm-6">
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="owner"
-                      onChange={e => {
-                        this.setVal('owner', e.target.checked)
-                      }} />
-                    <label className="form-check-label" htmlFor="owner">
-                      เจ้าของไปด้วย
+            <div className="form-group col-sm-12">
+              <div className="col-sm-6">
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" id="owner"
+                    onChange={e => {
+                      this.setVal('owner', e.target.checked)
+                    }} />
+                  <label className="form-check-label p-l-5" htmlFor="owner">
+                    เจ้าของไปด้วย
                   </label>
-                  </div>
-                </div>
-              </div>
-
-              <div className="form-group col-sm-12">
-                <label>ต้นทาง</label>
-                <input type="text" className="form-control" id="sourceAddress" onChange={
-                  e => {
-                    this.setVal('source.address', e.target.value)
-                  }
-                } />
-              </div>
-
-              <div className="form-group col-sm-12">
-                <label>ปลายทาง</label>
-                <input type="text" className="form-control" id="destinationAddress" onChange={
-                  e => {
-                    this.setVal('destination.address', e.target.value)
-                  }
-                } />
-              </div>
-
-              <div className="form-group col-sm-12">
-                <label>วันที่</label>
-                <input type="date" className="form-control" id="date" onChange={e => {
-                  this.setVal('date', new Date(e.target.value))
-                }} />
-              </div>
-
-              <div className="form-group col-sm-12">
-                <label>ช่องทางการชำระเงิน</label>
-                <div className="radio">
-                  <label>
-                    <input type="radio" name="optradio" style={{ margin: '5px' }} value={'line'} checked={service.payment === 'line'} onChange={e => { this.setVal('payment', e.target.value) }} />
-                    Line Pay
-                </label>
-                </div>
-                <div className="radio">
-                  <label><input type="radio" name="optradio" style={{ margin: '5px' }} value={'cash'} checked={service.payment === 'cash'} onChange={e => { this.setVal('payment', e.target.value) }} /> เงินสด</label>
-                </div>
-              </div>
-
-              <div className="form-group col-sm-12">
-                <label>เบอร์โทร</label>
-                <input type="text" className="form-control" id="customer_phone" placeholder="" onChange={e => {
-                  this.setVal('customer.phone', +e.target.value)
-                }} />
-              </div>
-
-              <div className="form-group col-sm-12">
-                <div className="container-login100-form-btn">
-                  <button className="login100-form-btn" onClick={this.onSubmit}>
-                    Submit
-                </button>
                 </div>
               </div>
             </div>
 
+            <div className="form-group col-sm-12">
+              <label>ต้นทาง</label>
+              <input type="text" className="form-control" id="sourceAddress" onChange={
+                e => {
+                  this.setVal('source.address', e.target.value)
+                }
+              } />
+            </div>
+
+            <div className="form-group col-sm-12">
+              <label>ปลายทาง</label>
+              <input type="text" className="form-control" id="destinationAddress" onChange={
+                e => {
+                  this.setVal('destination.address', e.target.value)
+                }
+              } />
+            </div>
+
+            <div className="form-group col-sm-12">
+              <label>วันที่</label>
+              <input type="date" className="form-control"
+                id="date" value={service.date} value={date} onChange={e => {
+                  this.setVal('date', new Date(e.target.value))
+                }} />
+            </div>
+
+            <div className="form-group col-sm-12">
+              <label>ช่องทางการชำระเงิน</label>
+              <div className="radio">
+                <label>
+                  <input type="radio" name="optradio" style={{ margin: '5px' }} value={'line'} checked={service.payment === 'line'} onChange={e => { this.setVal('payment', e.target.value) }} />
+                  Line Pay
+                </label>
+              </div>
+              <div className="radio">
+                <label><input type="radio" name="optradio" style={{ margin: '5px' }} value={'cash'} checked={service.payment === 'cash'} onChange={e => { this.setVal('payment', e.target.value) }} /> เงินสด</label>
+              </div>
+            </div>
+
+            <div className="form-group col-sm-12">
+              <label>เบอร์โทร</label>
+              <input type="text" className="form-control" id="customer_phone" placeholder="" onChange={e => {
+                this.setVal('customer.phone', +e.target.value)
+              }} />
+            </div>
+
+            <div className="form-group col-sm-12">
+              <div className="container-login100-form-btn">
+                <button className="login100-form-btn" onClick={this.onSubmit}>
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
         </DefaultLayout>
       </Fragment>
     )
