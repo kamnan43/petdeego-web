bc3dc7a337ed640d9ce3ceb849f1dbb65c6e513f
Gusjib <gusjib.j@gmail.com>
2018-12-28 19:14:20 +0700

    quotation

--- src/stores/QuotationStore.js
+++ src/stores/QuotationStore.js
@@ -8,6 +8,8 @@ class QuotationStore extends BaseStore {
     this.observable({
       loading: false,
       data: {},
+      price: 0,
+      driver: {},
     });
   }
 
@@ -15,16 +17,43 @@ class QuotationStore extends BaseStore {
     this.data = [];
   }
 
+  async setDriver(driver) {
+    this.driver = driver;
+  }
+
+  async setPrice(price) {
+    this.price = price;
+  }
+
   async getData(userId, orderId) {
     this.loading = true;
     let url = `${process.env.API_URL}/v1/quotation/${userId}/${orderId}`;
     let response = await http.get(url);
     if (response.statusCode === 200) {
-      let data = response.body.data;
-      delete data.data;
+      let data = response.body;
       this.data = data;
     } else {
-      this.data = [];
+      this.data = {};
+    }
+    this.loading = false;
+  }
+
+  async submit() {
+    this.loading = true;
+    let url = `${process.env.API_URL}/v1/quotation`;
+    let json = {
+      data: {
+        user_id: '2222222',
+        price: '200',
+        order_id: '12345',
+      }
+    }
+    let response = await http.post(url, json);
+    if (response.statusCode === 200) {
+      let data = response.body;
+      this.data = data;
+    } else {
+      this.data = {};
     }
     this.loading = false;
   }
