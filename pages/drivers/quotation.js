44573a51de34c0cd7184cc34cad3d69c986abeb9
Gusjib <gusjib.j@gmail.com>
2018-12-28 17:57:33 +0700

    add quotation

--- pages/drivers/quotation.js
+++ pages/drivers/quotation.js
@@ -3,6 +3,7 @@ import { observer, inject } from 'mobx-react';
 import querystring from 'query-string';
 import DefaultLayout from 'components/layout/DefaultLayout';
 import { withRouter } from 'next/router';
+import Header from 'components/form/Header';
 
 class Quotation extends Component {
   componentWillMount() {
@@ -20,48 +21,38 @@ class Quotation extends Component {
     if (quotation.data.length) {
       return (
         <DefaultLayout>
-          <div className="container-login100">
-            <div className="wrap-login100">
-              <div className="login100-form-title" style={{ backgroundImage: 'url(../home/images/bg-01.jpg)' }}>
-                <span className="login100-form-title-1">
-                  คุณได้เสนอราคาไปแล้ว
-                </span>
-              </div>
-              <form className="login100-form validate-form">
+          <Header title="Quotation" />
+          <div className="login100-form  row">
+            <form className="col-md-12">
+              <div className="form-group">
                 <div className="container-login100-form-btn">
                   <button className="login100-form-btn">
-                    ปิด
+                  ปิด
                   </button>
                 </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </DefaultLayout>
       )
     } else {
       return (
         <DefaultLayout>
-          <div className="container-login100">
-            <div className="wrap-login100">
-              <div className="login100-form-title" style={{ backgroundImage: 'url(../home/images/bg-01.jpg)' }}>
-                <span className="login100-form-title-1">
-                  Quotation
-                </span>
+          <Header title="Quotation" />
+          <div className="login100-form  row">
+            <form className="col-md-12">
+              <div className="form-group">
+                <label htmlFor="price">Price</label>
+                <input type="email" className="form-control" id="price" placeholder="0.00" />
               </div>
-              <form className="login100-form validate-form">
-                <div className="wrap-input100 validate-input m-b-26" data-validate="Price is required">
-                  <span className="label-input100">Price</span>
-                  <input className="input100" type="number" name="price" placeholder="0.00" />
-                  <span className="focus-input100" />
-                </div>
-  
+              <div className="form-group">
                 <div className="container-login100-form-btn">
                   <button className="login100-form-btn">
                     Submit
                   </button>
                 </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </DefaultLayout>
       )
